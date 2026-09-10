bf1f3b038e45b367f0be7b931c5c687c41ac240e
Abdo <your-email@example.com>
2026-09-10 06:24:43 +0300

    all orgin are avilable

--- icsrt-db/src/config/cors.ts
+++ icsrt-db/src/config/cors.ts
@@ -10,6 +10,7 @@ const productionDomains = getProductionDomains();
 
 export const allowedOrigins = envOrigins.length ? envOrigins : [
   ...productionDomains,
+  '*',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
